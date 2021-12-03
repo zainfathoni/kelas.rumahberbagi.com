c98ef8e5cb11b23a551ac02a7b550599f3d450d9
Zain Fathoni <me@zainf.dev>
2021-12-04 04:06:19 +0800

    feat: Simplify header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,32 +1,23 @@
 import { Link } from 'remix'
 
-/* This example requires Tailwind CSS v2.0+ */
-const navigation = [{ name: 'Beranda', href: '/' }]
-
 export function Header() {
   return (
     <header className="bg-indigo-600">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
-            <a href="#">
+            <Link prefetch="intent" to="/">
               <span className="sr-only">Workflow</span>
               <img
                 className="h-10 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
                 alt=""
               />
-            </a>
-            <div className="hidden ml-10 space-x-8 lg:block">
-              {navigation.map((link) => (
-                <Link key={link.name} to={link.href} className="text-base font-medium text-white hover:text-indigo-50">
-                  {link.name}
-                </Link>
-              ))}
-            </div>
+            </Link>
           </div>
           <div className="ml-10 space-x-4">
             <Link
+              prefetch="intent"
               to="/login"
               className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
             >
@@ -41,13 +32,6 @@ export function Header() {
               Daftar
             </a>
           </div>
-        </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map((link) => (
-            <Link key={link.name} to={link.href} className="text-base font-medium text-white hover:text-indigo-50">
-              {link.name}
-            </Link>
-          ))}
         </div>
       </nav>
     </header>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,6 +19,7 @@ export default function Example() {
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <Link
+              prefetch="intent"
               to="/login"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
             >
