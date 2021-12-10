56160562e33a024be9a3f41f659b46ed004177a0
Zain Fathoni <me@zainf.dev>
2021-12-11 06:25:46 +0800

    feat: Add a dashboard page

--- app/routes/magic.tsx
+++ app/routes/magic.tsx
@@ -9,7 +9,7 @@ export let loader: LoaderFunction = async ({ request }) => {
     // This redirect is optional, if not defined the user will be returnted by
     // the `authenticate` function and you can render something on this page
     // manually redirect the user.
-    successRedirect: '/me',
+    successRedirect: '/dashboard',
     // If something failed we take them back to the login page
     // This redirect is optional, if not defined any error will be throw and
     // the ErrorBoundary will be rendered.
