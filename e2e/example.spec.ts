c556eb06f12088cbd3f2165eef770f885df8016f
Zain Fathoni <me@zainf.dev>
2021-12-18 00:48:38 +0800

    test: Writing assertions
    https://playwright.dev/docs/intro#writing-assertions

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test'
 
-test('basic test', async ({ page }) => {
+test('my test', async ({ page }) => {
   await page.goto('https://playwright.dev/')
-  const title = page.locator('.navbar__inner .navbar__title')
-  await expect(title).toHaveText('Playwright')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/)
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro')
+
+  await page.click('text=Get Started')
+  // Expect some text to be visible on the page.
+  await expect(page.locator('text=Introduction').first()).toBeVisible()
 })
