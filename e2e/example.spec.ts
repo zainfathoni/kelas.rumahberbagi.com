560efb58efc8a0289c29b29c4bb9ccb24b575ebe
Zain Fathoni <me@zainf.dev>
2021-12-18 00:57:46 +0800

    test: Using test hooks
    https://playwright.dev/docs/intro#using-test-hooks

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,15 +1,20 @@
 import { test, expect } from '@playwright/test'
 
-test('my test', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+test.describe('Playwright', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('https://playwright.dev/')
+  })
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  test('my test', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/)
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro')
+    // Expect an attribute "to be strictly equal" to the value.
+    await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro')
 
-  await page.click('text=Get Started')
-  // Expect some text to be visible on the page.
-  await expect(page.locator('text=Introduction').first()).toBeVisible()
+    await page.click('text=Get Started')
+    // Expect some text to be visible on the page.
+    await expect(page.locator('text=Introduction').first()).toBeVisible()
+  })
 })
