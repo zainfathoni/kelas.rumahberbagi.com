37f945b1aa725808eeb1d679e58eca18683734ac
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2021-12-26 11:06:25 +0700

    feat: update seed.ts and enum.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,102 @@
 import { PrismaClient } from '@prisma/client'
-import { ROLES } from '../app/models/enum'
+import {
+  ROLES,
+  SUBSCRIPTION_STATUS,
+  TRANSACTION_STATUS,
+} from '../app/models/enum'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  await Promise.all(
-    getUsers().map((user) =>
-      prisma.user.create({
-        data: user,
-      })
-    )
-  )
-  await Promise.all(
+  // update or insert user with admin role
+  await prisma.user.upsert({
+    where: { email: 'me@zain.dev' },
+    update: {},
+    create: {
+      email: 'me@zainf.dev',
+      name: 'Zain',
+      phoneNumber: '+6512345678',
+      role: ROLES.ADMIN,
+    },
+  })
+
+  // update or insert user with author role
+  const author = await prisma.user.upsert({
+    where: { email: 'vika@rbagi.id' },
+    update: {},
+    create: {
+      email: 'vika@rbagi.id',
+      name: 'Vika',
+      role: ROLES.AUTHOR,
+    },
+  })
+
+  // update or insert user with member role
+  const member = await prisma.user.upsert({
+    where: { email: 'pk@zainf.dev' },
+    update: {},
+    create: {
+      email: 'pk@zainf.dev',
+      name: 'Pejuang Kode',
+      role: ROLES.MEMBER,
+    },
+  })
+
+  // create courses
+  const courses = await Promise.all(
     getCourse().map((course) =>
       prisma.course.create({
-        data: course,
+        data: {
+          authorId: author.id,
+          ...course,
+        },
       })
     )
   )
+
+  // create subscription
+  const subscription1 = await prisma.subscription.create({
+    data: {
+      userId: member.id,
+      courseId: courses[0].id,
+      status: SUBSCRIPTION_STATUS.ACTIVE,
+    },
+  })
+
+  // create subscription
+  const subscription2 = await prisma.subscription.create({
+    data: {
+      userId: member.id,
+      courseId: courses[1].id,
+      status: SUBSCRIPTION_STATUS.ACTIVE,
+    },
+  })
+
+  // create transaction with status SUBMITTED
+  await prisma.transaction.create({
+    data: {
+      userId: member.id,
+      subscriptionId: subscription1.id,
+      bankName: 'Bank Mandiri',
+      bankAccountName: 'Pejuang Kode',
+      bankAccountNumber: '123456789',
+      amount: 25000,
+      status: TRANSACTION_STATUS.SUBMITTED,
+    },
+  })
+
+  // create transaction with status VERIFIED
+  await prisma.transaction.create({
+    data: {
+      userId: member.id,
+      subscriptionId: subscription2.id,
+      bankName: 'Bank Mandiri',
+      bankAccountName: 'Pejuang Kode',
+      bankAccountNumber: '123456789',
+      amount: 50000,
+      status: TRANSACTION_STATUS.VERIFIED,
+    },
+  })
 }
 
 main()
@@ -29,34 +108,9 @@ main()
     await prisma.$disconnect()
   })
 
-function getUsers() {
-  return [
-    {
-      id: '82bf2c53-a9eb-4029-8d6d-17f514e8be7b',
-      email: 'me@zainf.dev',
-      name: 'Zain',
-      phoneNumber: '+6512345678',
-      role: ROLES.ADMIN,
-    },
-    {
-      id: '76ca6998-da6a-4523-bb71-397f7472cd49',
-      email: 'vika@rbagi.id',
-      name: 'Vika',
-      role: ROLES.AUTHOR,
-    },
-    {
-      id: 'ace81e7d-371a-42f4-99d5-2477237cc23d',
-      email: 'pk@zainf.dev',
-      name: 'Pejuang Kode',
-      role: ROLES.MEMBER,
-    },
-  ]
-}
-
 function getCourse() {
   return [
     {
-      authorId: '76ca6998-da6a-4523-bb71-397f7472cd49',
       name: 'Menumbuhkan Minat Baca Anak',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -65,7 +119,6 @@ function getCourse() {
       category: 'Parenting',
     },
     {
-      authorId: '76ca6998-da6a-4523-bb71-397f7472cd49',
       name: 'Mengelola Konflik dalam Keluarga',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -74,7 +127,6 @@ function getCourse() {
       category: 'Parenting',
     },
     {
-      authorId: '76ca6998-da6a-4523-bb71-397f7472cd49',
       name: 'Menumbuhkan Rasa Percaya Diri Sejak Dini',
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
