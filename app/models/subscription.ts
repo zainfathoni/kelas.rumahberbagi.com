4e589a2c0453d12a89effee1c848480305652805
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2021-12-27 12:01:25 +0700

    fix: edit 'active' text to using constant value

--- app/models/subscription.ts
+++ app/models/subscription.ts
@@ -1,10 +1,11 @@
+import { SUBSCRIPTION_STATUS } from './enum'
 import { db } from '~/utils/db.server'
 
 export async function getSubscriptionActiveByUserId(userId: string) {
   return await db.subscription.findFirst({
     where: {
       userId,
-      status: 'active',
+      status: SUBSCRIPTION_STATUS.ACTIVE,
     },
   })
 }
