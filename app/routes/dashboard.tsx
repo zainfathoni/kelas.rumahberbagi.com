059ef14ca304d1e1db9ebd16ee8ee5f262d0523b
Zain Fathoni <me@zainf.dev>
2021-12-29 13:05:46 +0800

    feat: replace Avatar with a Circular avatar with placeholder initials

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -156,11 +156,11 @@ export default function Dashboard() {
                   >
                     <div className="flex items-center">
                       <div>
-                        <img
-                          className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
+                        <span className="inline-flex items-center justify-center h-10 w-10 rounded-full bg-gray-500">
+                          <span className="text-medium font-medium leading-none text-white">
+                            TC
+                          </span>
+                        </span>
                       </div>
                       <div className="ml-3">
                         <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
@@ -234,11 +234,11 @@ export default function Dashboard() {
               >
                 <div className="flex items-center">
                   <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
+                    <span className="inline-flex items-center justify-center h-10 w-10 rounded-full bg-gray-500">
+                      <span className="text-medium font-medium leading-none text-white">
+                        TC
+                      </span>
+                    </span>
                   </div>
                   <div className="ml-3">
                     <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
