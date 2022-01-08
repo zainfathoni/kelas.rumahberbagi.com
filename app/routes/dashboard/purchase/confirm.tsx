46847b57d74ddd8130960c5bedc5cf5bd13af1c2
AchmadWahyu <awahyu253@gmail.com>
2022-01-09 00:27:39 +0700

    feat: added getFirstCourse and debugging action onSubmit

--- app/routes/dashboard/purchase/confirm.tsx
+++ app/routes/dashboard/purchase/confirm.tsx
@@ -1,9 +1,10 @@
-import { Outlet, redirect } from 'remix'
+import { Form, Outlet, redirect } from 'remix'
 import type { ActionFunction, LoaderFunction } from 'remix'
 import type { Subscription, User } from '@prisma/client'
 import { validatePhoneNumber, validateRequired } from '~/utils/validators'
 import { auth } from '~/services/auth.server'
 import { db } from '~/utils/db.server'
+import { getFirstCourse } from '~/models/course'
 
 interface TransactionFields {
   userId: string
@@ -31,9 +32,20 @@ type ActionData = {
 }
 
 export const action: ActionFunction = async ({ request }) => {
+  console.log('action called')
   const user = await auth.isAuthenticated(request, {
     failureRedirect: '/login',
   })
+
+  console.log('user: ', user)
+  const course = await getFirstCourse()
+  console.log('course: ', course)
+
+  if (!course) {
+    throw new Response('Course not found', {
+      status: 404,
+    })
+  }
 
   const form = await request.formData()
   const name = form.get('name')
@@ -43,7 +55,8 @@ export const action: ActionFunction = async ({ request }) => {
   const bankAccountName = form.get('bankAccountName')
   const amount = form.get('amount')
   const paymentTime = form.get('paymentTime')
-
+  const parsedAmount: number = amount ? parseInt(amount, 10) : 0
+  console.log('parsedAmount types: ', typeof parsedAmount)
   if (
     typeof name !== 'string' ||
     typeof phoneNumber !== 'string' ||
@@ -63,13 +76,13 @@ export const action: ActionFunction = async ({ request }) => {
     bankAccountNumber: validateRequired('Nama Bank', bankAccountNumber),
     bankAccountName: validateRequired('Nomor Rekening', bankAccountName),
     amount: validateRequired('Nominal', amount),
-    paymentTime: validateRequired('Nominal', paymentTime),
+    paymentTime: validateRequired('Tanggal dan Waktu Pembayaran', paymentTime),
   }
 
   const fields: TransactionFields = {
     userId: user.id,
-    courseId: '123',
-    authorId: '123',
+    courseId: course.id,
+    authorId: course.authorId,
     bankName,
     bankAccountName,
     bankAccountNumber,
@@ -79,8 +92,10 @@ export const action: ActionFunction = async ({ request }) => {
   if (Object.values(fieldErrors).some(Boolean)) {
     return { fieldErrors, fields }
   }
+  console.log('fields: ', fields)
 
   const transaction = await db.transaction.create({ data: fields })
+  console.log('transaction: ', transaction)
 
   if (!transaction) {
     throw new Response('Error while creating new transaction', {
@@ -88,9 +103,59 @@ export const action: ActionFunction = async ({ request }) => {
     })
   }
 
-  return redirect(`/dashboard/transactions/${transaction.id}`)
+  return redirect(`/dashboard/purchase/confirm/${transaction.id}`)
 }
 
 export default function Confirm() {
-  return <Outlet />
+  return (
+    <>
+      <Outlet />
+      <Form action="/dashboard/purchase/confirm" method="post">
+        <div>
+          <label>
+            Nama Lengkap Anda: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nomor WhatsApp Anda: <input type="text" name="phoneNumber" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nama Bank: <input type="text" name="bankName" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nomor Rekening: <input type="text" name="bankAccountNumber" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nama Pemilik Rekening: <input type="text" name="bankAccountName" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nominal: <input type="text" name="amount" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Tanggal dan Waktu Pembayaran:
+            <input type="datetime-local" id="meeting-time" name="paymentTime" />
+          </label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Konfirmasi Pembayaran
+          </button>
+        </div>
+      </Form>
+    </>
+  )
 }
