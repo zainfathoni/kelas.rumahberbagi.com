1dd7aebf74179be6259285c97ebaf6691676eb92
Zain Fathoni <me@zainf.dev>
2022-02-26 14:49:52 +0800

    fix: formatDateTime

--- app/utils/format.ts
+++ app/utils/format.ts
@@ -2,5 +2,7 @@ export const formatDateTime = (date: Date) => {
   return date
     .toLocaleString('en-CA')
     .replace(/, /g, 'T')
+    .replace(/T(\d):/g, 'T0$1:')
+    .replace(/:(\d):/g, ':0$1:')
     .replace(/:\d\d .*$/, '')
 }
