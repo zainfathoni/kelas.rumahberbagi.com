42e75c4c72568603a4a7024dfde4852494e0f724
Zain Fathoni <me@zainf.dev>
2022-02-28 02:34:27 +0800

    feat: copy contained breadcrumbs from Tailwind UI
    https://tailwindui.com/components/application-ui/navigation/breadcrumbs

---
+++ app/components/breadcrumbs.tsx
@@ -0,0 +1,48 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { HomeIcon } from '@heroicons/react/solid'
+import { Link } from 'remix'
+
+const pages = [
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Project Nero', href: '#', current: true },
+]
+
+export default function Example() {
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol className="bg-white rounded-md shadow px-6 flex space-x-4">
+        <li className="flex">
+          <div className="flex items-center">
+            <Link to="/" className="text-gray-400 hover:text-gray-500">
+              <HomeIcon className="flex-shrink-0 h-5 w-5" aria-hidden="true" />
+              <span className="sr-only">Home</span>
+            </Link>
+          </div>
+        </li>
+        {pages.map((page) => (
+          <li key={page.name} className="flex">
+            <div className="flex items-center">
+              <svg
+                className="flex-shrink-0 w-6 h-full text-gray-200"
+                viewBox="0 0 24 44"
+                preserveAspectRatio="none"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+              </svg>
+              <a
+                href={page.href}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? 'page' : undefined}
+              >
+                {page.name}
+              </a>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  )
+}
