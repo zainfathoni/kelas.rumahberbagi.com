dfc5a4b3570c4782f9d7fb25db58043000564875
Zain Fathoni <me@zainf.dev>
2022-03-01 13:33:15 +0800

    feat: add a link to the tutorial video

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
               description="Agar kelas ini nyaman diikuti dan mudah untuk dirujuk kembali di kemudian hari, maka saya sediakan:"
               image={
                 <BenefitContainerImage
-                  src="/images/planner-preview.jpeg"
+                  src="/images/interaction-preview.jpeg"
                   alt="Halaman perencanaan"
                   height={1280}
                   width={937}
@@ -85,12 +85,37 @@ export default function HomePage() {
               title="Interaksi dua arah"
               description="Selain materi di atas yang bersifat satu arah, kami juga menyediakan sarana interaksi dua arah."
               image={
-                <BenefitContainerImage
-                  src="/images/interaction-preview.jpeg"
-                  alt="Materi interaksi di kelas"
-                  height={959}
-                  width={1280}
-                />
+                <a
+                  href="https://rbagi.id/tutorial"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <BenefitContainerImage
+                    src="/images/tutorial-preview.jpeg"
+                    alt="Materi interaksi di kelas"
+                    height={959}
+                    width={1280}
+                  />
+                  <div
+                    className="absolute inset-0 w-full h-full flex items-center justify-center"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="h-20 w-20 text-indigo-500"
+                      fill="currentColor"
+                      viewBox="0 0 84 84"
+                    >
+                      <circle
+                        opacity="0.9"
+                        cx="42"
+                        cy="42"
+                        r="42"
+                        fill="white"
+                      />
+                      <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+                    </svg>
+                  </div>
+                </a>
               }
             >
               <BenefitItem icon={<TelegramIcon />} title="Grup Telegram">
