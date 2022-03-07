9320df12256e77b8c2883035d921af19f0f97d92
Zain Fathoni <me@zainf.dev>
2022-03-07 22:22:33 +0800

    feat: add back to directory navigation

--- app/routes/dashboard/course/$lessonId.tsx
+++ app/routes/dashboard/course/$lessonId.tsx
@@ -35,31 +35,34 @@ export default function LessonPage() {
   const { lesson } = useLoaderData<{ lesson: Lesson }>()
 
   return (
-    <article>
+    <div className="flex flex-col h-screen w-full">
       <nav
-        className="flex items-start px-4 py-3 sm:px-6 lg:px-8 xl:hidden"
+        className="flex items-start px-4 py-3 sm:px-6 lg:px-8 xl:hidden border-solid border-y-2 border-gray-200"
         aria-label="Breadcrumb"
       >
         <Link
           to="/dashboard/course"
           className="inline-flex items-center space-x-3 text-sm font-medium text-gray-900"
         >
+          <span className="absolute inset-0" aria-hidden="true" />
           <ChevronLeftIcon
             className="-ml-2 h-5 w-5 text-gray-400"
             aria-hidden="true"
           />
-          <span>Directory</span>
+          <span>Kembali</span>
         </Link>
       </nav>
-      <div className="m-8 aspect-w-16 aspect-h-9">
-        <iframe
-          title={lesson.name}
-          src={`https://www.youtube.com/embed/${lesson.videoId}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </article>
+      <article className="h-full">
+        <div className="m-8 aspect-w-16 aspect-h-9">
+          <iframe
+            title={lesson.name}
+            src={`https://www.youtube.com/embed/${lesson.videoId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </article>
+    </div>
   )
 }
