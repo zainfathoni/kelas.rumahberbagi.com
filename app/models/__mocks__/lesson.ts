3896718166c72a2566b0ec50af8953e4bcea1875
Zain Fathoni <me@zainf.dev>
2022-03-07 22:44:50 +0800

    feat: render video name and description

--- app/models/__mocks__/lesson.ts
+++ app/models/__mocks__/lesson.ts
@@ -5,8 +5,8 @@ export const lessonBuilder = build<
   Omit<Lesson, 'id' | 'createdAt' | 'updatedAt' | 'chapterId'>
 >({
   fields: {
-    name: fake((f) => f.commerce.productName()),
-    description: fake((f) => f.commerce.productDescription()),
+    name: fake((f) => f.lorem.sentence()),
+    description: fake((f) => f.lorem.paragraphs()),
     videoId: 'G3ZS8x86588', // https://www.youtube.com/watch?v=G3ZS8x86588
     order: fake((f) => f.datatype.number()),
   },
--- app/routes/dashboard/course/$lessonId.tsx
+++ app/routes/dashboard/course/$lessonId.tsx
@@ -62,6 +62,20 @@ export default function LessonPage() {
             allowFullScreen
           ></iframe>
         </div>
+        <div className="mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+          <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+            <div className="sm:col-span-1">
+              <dt className="text-sm font-medium text-gray-500">Judul</dt>
+              <dd className="mt-1 text-sm text-gray-900">{lesson.name}</dd>
+            </div>
+            <div className="sm:col-span-2">
+              <dt className="text-sm font-medium text-gray-500">Deskripsi</dt>
+              <dd className="mt-1 max-w-prose text-sm text-gray-900 space-y-5">
+                {lesson.description}
+              </dd>
+            </div>
+          </dl>
+        </div>
       </article>
     </div>
   )
